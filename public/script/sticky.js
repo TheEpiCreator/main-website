bda204b9071ba825858f30ea4f79dc19f499d65c
TheEpiCreator <53312465+TheEpiCreator@users.noreply.github.com>
2020-08-29 22:36:53 -0500

    Add sticky.js

---
+++ public/script/sticky.js
@@ -0,0 +1,22 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = () => { sticky() }
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function sticky() {
+    //update sticky elements
+    header = document.getElementById("sticky")
+    mainImg = document.getElementById("topContainer")
+
+    //if window scrolls past top of header
+    if (window.pageYOffset > mainImg.offsetHeight) {
+        //make header sticky
+        header.classList.add("sticky")
+        //activate spacer
+        spacer.classList.remove("invisible")
+    } else {
+        //unstick header
+        header.classList.remove("sticky")
+        //deactivate spacer
+        spacer.classList.add("invisible")
+    }
+} 
